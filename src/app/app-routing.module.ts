689b127b0b04b1dc04702d4df9cd65f8d2a0cea9
seeduardo <romenflatiron@gmail.com>
2019-04-25 23:42:16 +0100

    Added default route to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AddRiffTemplateComponent } from './add-riff-template/add-riff-template.
 import { AddRiffModelComponent } from './add-riff-model/add-riff-model.component'
 
 const routes: Routes = [
+  {path: '', component: RiffsComponent},
   {path: 'riffs', component: RiffsComponent},
   {path: 'add-riff-template', component: AddRiffTemplateComponent},
   {path: 'add-riff-model', component: AddRiffModelComponent}
