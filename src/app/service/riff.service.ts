05ed8f7936a87daa39928ba491134598b7719e73
seeduardo <romenflatiron@gmail.com>
2019-05-11 18:07:20 +0100

    Started refactoring riffService to use simulated server via httpClient

--- src/app/service/riff.service.ts
+++ src/app/service/riff.service.ts
@@ -3,20 +3,23 @@ import { MockData } from './../mock-data/mock-riff-data';
 import { Riff } from '../models/riff';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RiffService {
 
-  riffs: Riff[] = [];
+  // riffs: Riff[] = [];
+  riffsUrl = 'api/riffs';
 
-  constructor() {
-    this.riffs = MockData.Riffs;
+  constructor(private httpClient: HttpClient) {
+    // this.riffs = MockData.Riffs;
   }
 
   getRiffs(): Observable<Riff[]> {
-    return of(this.riffs);
+    // return of(this.riffs);
+    return this.httpClient.get<Riff[]>(this.riffsUrl)
   }
 
   getRiff(id: number): Observable {
